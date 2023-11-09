8269af1a75efeaad8f29a0ae494453d0694039f6
gedeonagmas <103260131+gedeonagmas@users.noreply.github.com>
2023-11-09 15:49:56 +0300

    Update errorController.js

--- server/controller/errorController.js
+++ server/controller/errorController.js
@@ -27,10 +27,16 @@ const prodError = (err, res) => {
       .status(err.statusCode)
       .json({ status: err.status, message: err.message });
   } else {
-    res.status(500).json({
-      status: "error",
-      message: "something went wrong please refresh the page!",
-    });
+    // res.status(500).json({
+    //   status: "error",
+    //   message: "something went wrong please refresh the page!",
+    // });
+    res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    stack: err.stack,
+    message: err.message,
+  });
   }
 };
 
