aa4f4030b5ed6fa5c98ded9b9401afb8693c0760
gedeonagmas <103260131+gedeonagmas@users.noreply.github.com>
2023-11-09 14:05:53 +0300

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const mongodb = async () => {
-  await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }); //replace by MONGO_URL
+  await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }); //replace by MONGO_URL
   console.log("gym database connected successfully");
 };
 
