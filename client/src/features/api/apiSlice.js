aea422373a51e61d7332915c2897a55880df8709
gedeonagmas <103260131+gedeonagmas@users.noreply.github.com>
2023-11-09 14:27:21 +0300

    Update apiSlice.js

--- client/src/features/api/apiSlice.js
+++ client/src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "/gym/app/v1", //this will be render api url + /gym/app/v1
+    baseUrl: "https://gedi.vercel.app/gym/app/v1", //this will be render api url + /gym/app/v1
   }),
   tagTypes: [
     "profile-info",
